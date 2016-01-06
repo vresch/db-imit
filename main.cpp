0f92360ea9ff32013fd51d9fd7c3d39ae9bb160a
Max Vresch <max.vresch@gmail.com>
2016-01-07 00:49:33 +0200

    make functions with struct, vector & set as args

--- main.cpp
+++ main.cpp
@@ -1,55 +1,67 @@
-/* DB imitation*/
-// make functions for display(), file_read(), select(), etc..
+/*  DB imitation*/
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
 #include <set>
 
-struct city {
- int id;
- std::string value;
+struct City {
+    int id;
+    std::string value;
 };
 
+City line_to_struct(std::ifstream &ifs);
+std::vector<City> file_to_vector(const std::string filename, std::vector<City> &cities);
+void display_vector(const std::vector<City> &cities, const std::set<int> &selected_cities);
+
 int main() {
- std::vector<city> citydb;
- std::ifstream ifs; // possible to open file in construcor
- ifs.open("city.db", std::ifstream::in);
- /* read file by lines and push them in vector */
- while( !ifs.eof() ) {
-  city inc;
-  std::string id;
-  std::getline(ifs, id, '|');
-  inc.id = stoi(id);
-  //std::cout << inc.id << " |"; // check file read by line to struct object
-  std::getline(ifs, inc.value);
-  //std::cout << inc.value << '\n'; // check file read by line to struct object
-  
-  citydb.push_back(inc);
-  std::cout << citydb.back().id << " |" << citydb.back().value << '\n'; // check vector pushing
- }
- ifs.close(); // finished file read to vector and close file stream
+    std::vector<City> cities;
+    std::set<int> selected_cities;
+    std::string filename = "city.db";
+    cities = file_to_vector(filename, cities);
+    
+    int sel = 1;
+    while (sel) {
+        display_vector(cities, selected_cities);
+        std::cout << "Input ID Number of item to select/deselect (0 - to quit): ";
+        std::cin >> sel;
+        if ( selected_cities.find(sel) != selected_cities.end() )
+            selected_cities.erase(sel);
+        else
+            selected_cities.insert(sel); // make check if SEL is in vector cities
+    }
 
- /* selection, make function */
- std::set<int> citysel;
- int sel = 1;
- while (sel) {
-  std::cout << "Input ID Number of item to select/deselect (0 - to quit): ";
-  std::cin >> sel;
-  if ( citysel.find(sel) != citysel.end() )
-   citysel.erase(sel);
-  else
-   citysel.insert(sel); // make check if SEL is in vector CITYDB
- 
-  /* print vector with selection/deselection, make function display() */
-  for(int i = 0; i < citydb.size(); i++) {
-   if ( citysel.count(citydb.at(i).id) != 0)
-    std::cout << '*';
-   else
-    std::cout << ' ';
-   std::cout << citydb.at(i).id << " |" << citydb.at(i).value << '\n';
-  }
- }
+    return 0;
+}
 
- return 0;
+std::vector<City> file_to_vector(const std::string filename, std::vector<City> &cities) {
+    std::ifstream ifs(filename, std::ifstream::in);
+    while( !ifs.eof() ) {
+        City city;
+        city = line_to_struct(ifs);
+        cities.push_back(city);
+    }
+    ifs.close();
+
+    return cities;
+}
+
+City line_to_struct(std::ifstream &ifs) {
+    City city;
+    std::string id;
+    std::getline(ifs, id, '|');
+    city.id = stoi(id);
+    std::getline(ifs, city.value);
+
+    return city;
+}
+
+void display_vector(const std::vector<City> &cities, const std::set<int> &selected_cities) {
+    for(int i = 0; i < cities.size(); i++) {
+        if (selected_cities.count(cities.at(i).id) != 0)
+            std::cout << '*';
+        else
+            std::cout << ' ';
+        std::cout << cities.at(i).id << " |" << cities.at(i).value << '\n';
+    }
 }
